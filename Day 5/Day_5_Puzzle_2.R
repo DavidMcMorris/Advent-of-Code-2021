fc0ee9438caed28c001e26611ff8e6465fd9368d
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2021-12-05 21:44:01 -0500

    Puzzle 2

---
+++ Day 5/Day_5_Puzzle_2.R
@@ -0,0 +1,44 @@
+#Day 5, Puzzle 2
+
+
+input <- read.table("Day_5_input.txt", colClasses="character")
+
+input <- data.frame(input$V1,input$V3)
+colnames(input) <- c("start","end")
+
+start_mat <- matrix(nrow = nrow(input),do.call('rbind', strsplit(as.character(input$start),split=',')))
+end_mat <- matrix(nrow = nrow(input),do.call('rbind', strsplit(as.character(input$end),split=',')))
+
+start_mat <- matrix(as.numeric(start_mat),nrow=nrow(input))
+end_mat <- matrix(as.numeric(end_mat),nrow=nrow(input))
+
+v_inds <- which(start_mat[,1] == end_mat[,1])
+h_inds <- which(start_mat[,2] == end_mat[,2])
+d_inds <- setdiff(1:500,c(v_inds,h_inds))
+
+h_lines <- cbind(start_mat[h_inds,],end_mat[h_inds,])
+v_lines <- cbind(start_mat[v_inds,],end_mat[v_inds,])
+d_lines <- cbind(start_mat[d_inds,],end_mat[d_inds,])
+
+h_coords <- NULL
+for(i in 1:nrow(h_lines)){
+	h_coords <- rbind(h_coords,cbind(h_lines[i,1]:h_lines[i,3],rep(h_lines[i,2],abs(h_lines[i,3] - h_lines[i,1])+1)))
+}
+
+v_coords <- NULL
+for(i in 1:nrow(v_lines)){
+	v_coords <- rbind(v_coords,cbind(rep(v_lines[i,1],abs(v_lines[i,4] - v_lines[i,2])+1),v_lines[i,2]:v_lines[i,4]))
+}
+
+d_coords <- NULL
+for(i in 1:nrow(d_lines)){
+	d_coords <- rbind(d_coords,cbind(d_lines[i,1]:d_lines[i,3],d_lines[i,2]:d_lines[i,4]))
+}
+
+
+coords <- rbind(h_coords,v_coords,d_coords)
+strings <- paste(coords[,1],coords[,2])
+
+number <- sum(table(strings)>=2)
+
+print(number)

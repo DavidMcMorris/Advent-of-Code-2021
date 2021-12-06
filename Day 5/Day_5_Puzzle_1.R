4e643f319160e491981c6923da7f7325f3353bb0
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2021-12-05 21:02:00 -0500

    Puzzle 1
    Not a very good solution, but a solution

--- Day 5/Day_5_Puzzle_1.R
+++ Day 5/Day_5_Puzzle_1.R
@@ -2,7 +2,6 @@
 
 
 input <- read.table("Day_5_input.txt", colClasses="character")
-# input <- read.table("test_input.txt", colClasses="character")
 
 input <- data.frame(input$V1,input$V3)
 colnames(input) <- c("start","end")
@@ -13,11 +12,11 @@ end_mat <- matrix(nrow = nrow(input),do.call('rbind', strsplit(as.character(inpu
 start_mat <- matrix(as.numeric(start_mat),nrow=nrow(input))
 end_mat <- matrix(as.numeric(end_mat),nrow=nrow(input))
 
-h_inds <- which(start_mat[,1] == end_mat[,1])
-v_inds <- which(start_mat[,2] == end_mat[,2])
+v_inds <- which(start_mat[,1] == end_mat[,1])
+h_inds <- which(start_mat[,2] == end_mat[,2])
 
-h_lines <- cbind(start_mat[v_inds,],end_mat[v_inds,])
-v_lines <- cbind(start_mat[h_inds,],end_mat[h_inds,])
+h_lines <- cbind(start_mat[h_inds,],end_mat[h_inds,])
+v_lines <- cbind(start_mat[v_inds,],end_mat[v_inds,])
 
 h_coords <- NULL
 for(i in 1:nrow(h_lines)){
@@ -29,9 +28,20 @@ for(i in 1:nrow(v_lines)){
 	v_coords <- rbind(v_coords,cbind(rep(v_lines[i,1],abs(v_lines[i,4] - v_lines[i,2])+1),v_lines[i,2]:v_lines[i,4]))
 }
 
+
 coords <- rbind(h_coords,v_coords)
 strings <- paste(coords[,1],coords[,2])
 
-number <- sum(table(strings)==max(table(strings)))
+x <- 0
+for(i in 1:length(strings)){
+	x <- c(x,sum(strings == strings[i]))
+}
+
+max_overlaps <- max(x)
+
+number <- 0
+for(i in 2:max_overlaps){
+	number <- number + sum(x==i)/i
+}
 
 print(number)

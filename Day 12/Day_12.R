c4f4e4f15013d01609517c53235f475827fbb2af
DavidMcMorris <david.mcmorris@huskers.unl.edu>
2022-06-04 16:20:38 -0400

    Finished Part 2

--- Day 12/Day_12.R
+++ Day 12/Day_12.R
@@ -1,3 +1,5 @@
+Part <- 2 #Set 1 or 2 for Part 1 or 2
+
 input<-read.table("Day_12_Input.txt",sep='-',stringsAsFactors=TRUE)
 lvls <- factor(unique(c(input$V1,input$V2)))
 
@@ -19,6 +21,14 @@ caves[inds]<-1
 start <- unique(as.matrix(input)[start])
 end <- unique(as.matrix(input)[end])
 
+small_caves <- setdiff(which(caves==0),c(start,end))
+
+if(Part == 1){
+	caves[small_caves] <- 0
+}
+if(Part == 2){
+	caves[small_caves] <- 2
+}
 
 paths <- 0
 
@@ -29,15 +39,33 @@ path_finder <- function(current,adj,paths,caves){
 		}
 		return(paths)
 	}
+
+	if(caves[current]==3){
+		for(i in 1:length(caves)){
+			adj[[i]] <- setdiff(adj[[i]],current)
+		}
+		for(j in which(caves==3)){
+			for(i in 1:length(caves)){
+				adj[[i]] <- setdiff(adj[[i]],j)
+			}
+		}
+		caves[which(caves==2)] <- 0
+	}
+
 	if(caves[current] == 0){
 		for(i in 1:length(caves)){
 			adj[[i]] <- setdiff(adj[[i]],current)
 		}
 	}
 
+	if(caves[current] == 2){
+		caves[current]<-3
+	}
+
 	for(i in 1:length(adj[[current]])){
 		paths <- path_finder(adj[[current]][i],adj,paths,caves)
 	}
+
 	return(paths)
 }
 
